5fa47874a3560ef906d9a52d0dc0bc9f494728e7
qwerty <mauserkurz98@gmail.com>
2021-03-08 09:51:15 +0800

    update dist folder and correct build path

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: path.resolve(__dirname, './src/app/index.js'),
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './dist/app'),
     filename: 'bundle.js',
     publicPath: "/app/"
   },

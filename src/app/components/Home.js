3c4ddeb5c02275b3abe9b196eefb17c8d2b553eb
qwerty <mauserkurz98@gmail.com>
2021-03-08 16:30:03 +0800

    change state on click event

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export class Home extends React.Component {
+export default class Home extends React.Component {
   constructor(props) {
     super();
-    this.age = props.age;
+    this.state = {
+      age: props.age,
+      status: 0,
+    };
   }
 
   onMakeOlder() {
-    this.age += 3;
+    this.setState({
+      age: this.state.age + 3,
+    });
   }
 
   render() {
     return (
       <div>
         <p>In a new Component!</p>
-        <p>Your name is { this.props.name }, your age is { this.age }</p>
+        <p>Your name is { this.props.name }, your age is { this.state.age }</p>
+        <p>Status: {this.state.status}</p>
         <hr/>
         <button
           onClick={() => this.onMakeOlder()}
--- src/app/index.js
+++ src/app/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
-import { Header } from "./components/Header";
-import { Home } from "./components/Home";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 class App extends React.Component {
   render() {
